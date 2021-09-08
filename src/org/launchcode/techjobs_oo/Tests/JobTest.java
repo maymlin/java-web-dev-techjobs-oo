f0a6902ac2b53e2c257e92133bbbb15db1562bbe
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-08 18:47:17 -0500

    Complete #4 Use Unit Testing to Verify Parts of the Job Class

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,49 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.Employer;
+import org.launchcode.techjobs_oo.Location;
+import org.launchcode.techjobs_oo.PositionType;
+import org.launchcode.techjobs_oo.CoreCompetency;
+
+public class JobTest {
+    Job testJob;
+
+    @Before
+    public void setUp() {
+        testJob = new Job();
+    }
+
+    @Test
+    public void testSettingJobId() {
+        Job secondJob = new Job();
+        assertEquals(2, secondJob.getId(), 0.001);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job newJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType(
+                "Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(newJob instanceof Job);
+        assertEquals("Product tester", newJob.getName());
+        assertEquals("ACME", newJob.getEmployer().getValue());
+        assertEquals("Desert", newJob.getLocation().getValue());
+        assertEquals("Quality control", newJob.getPositionType().getValue());
+        assertEquals("Persistence", newJob.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job newJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType(
+                "Quality control"), new CoreCompetency("Persistence"));
+        Job duplicateJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType(
+                "Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(newJob.equals(duplicateJob));
+    }
+
+}
